f9120efb943aef5a407f6e754c9467ebc137a1ca
Britt Carlton <brittanyrc95@gmail.com>
2022-08-12 12:14:47 -0400

    Add route to get a single product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
   Product.findAll({
     include: [Category, Tag]
   })
@@ -21,6 +19,20 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({ where: { id: req.params.id },
+    include: [Category, Tag]
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+        res.json(404).json({ message: 'No product found with that id' });
+        return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({ message: err });
+  });
 });
 
 // create new product
